b107878d41fe8e779e06f28170b89e1d6869f451
Klark Sisperez <klarkjasper.sisperez@tup.edu.ph>
2022-06-08 22:03:17 +0800

    dynamic deployment

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const WebSocket = require('ws');
 const path = require('path');
 const EventHubReader = require('./scripts/event-hub-reader.js');
 
-const iotHubConnectionString = process.env.IotHubConnectionString;
+const iotHubConnectionString = process.env.IotHubConnectionString || "HostName=raspi3-monitoring.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=P9yYhI3nKYwlhS0Pb0P6rmUd525L6G0JbSDZpsTf9wU=";
 if (!iotHubConnectionString) {
   console.error(`Environment variable IotHubConnectionString must be specified.`);
   return;
 }
 console.log(`Using IoT Hub connection string [${iotHubConnectionString}]`);
 
-const eventHubConsumerGroup = process.env.EventHubConsumerGroup;
+const eventHubConsumerGroup = process.env.EventHubConsumerGroup || "telConsumerGroup";
 console.log(eventHubConsumerGroup);
 if (!eventHubConsumerGroup) {
   console.error(`Environment variable EventHubConsumerGroup must be specified.`);
